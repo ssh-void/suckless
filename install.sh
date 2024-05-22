ab5f968c5a605ad6f86c8e98461539c610b9ecc6
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-05-22 20:18:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-sudo xbps-install -Syu build-essential xorg libxrandr-dev libx11-dev libxft-dev libxdo-dev libxinerama-dev libfribidi-dev libharfbuzz-dev
-libx11-dev libxinerama-dev  libxft-dev libx11-xcb-dev libxcb-res0-dev 
+sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
+sudo xbps-install -S xorg-minimal base-devel xrandr setxkbmap libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel gcc python feh libXrandr-devel nerd-fonts 
+sudo rm /var/service/agetty-tty3
+sudo rm /var/service/agetty-tty4
+sudo rm /var/service/agetty-tty5
+sudo rm /var/service/agetty-tty6
+fc-cache -fv
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+sudo sed -i 's/GETTY_ARGS="--noclear"/GETTY_ARGS="--noclear --autologin rpi"/' /etc/runit/runsvdir/current/agetty-tty1/conf
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo xbps-remove -Oy && sudo xbps-remove -oy
+sudo reboot
+
